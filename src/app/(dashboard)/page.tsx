c23993525c7a9297c0dfc0b634f90b731dfcdd30
Mostafa3lyan <96500374+Mostafa3lyan@users.noreply.github.com>
2025-12-14 02:55:00 +0200

    initial commit

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,4 @@
+// app/(home)/page.tsx
+export default function HomePage() {
+  return <h1>Welcome to Exam App</h1>;
+}
---
+++ src/app/error.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function ErrorPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-3xl font-bold">Error</p>
+    </div>
+  );
+}
